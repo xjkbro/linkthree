2fd301b056a7dffccc3f93201ec70e5b6a9d4e5c
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-06-02 10:48:48 -0700

    init

---
+++ constants/links.js
@@ -0,0 +1,39 @@
+export const LINKS = [
+    {
+        "name": "Website",
+        "url": "https://www.jkdelara.com/"
+    },
+    {
+        "name": "LinkedIn",
+        "url": "https://www.linkedin.com/in/jkdelara/"
+    },
+    {
+        "name": "GitHub",
+        "url": "https://www.github.com/xjkbro/"
+    },
+    {
+        "name": "Twitter",
+        "url": "https://www.twitter.com/json_delara/"
+    },
+    {
+        "name": "Instagram",
+        "url": "https://www.instagram.com/jkbro/"
+    },
+    {
+        "name": "YouTube",
+        "url": "https://www.youtube.com/@xjustkiddingbro/"
+    },
+    {
+        "name": "Twitch",
+        "url": "https://www.twitch.tv/xjkbro/"
+    },
+    
+    {
+        "name": "Unsplash",
+        "url": "https://www.unsplash.com/@jkdelara/"
+    },
+    {
+        "name": "TikTok",
+        "url": "https://www.tiktok.com/@xjkbro/"
+    }
+]
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['localhost', 'res.cloudinary.com','scontent-lax3-2.cdninstagram.com'],
+    }
+}
 
 module.exports = nextConfig
