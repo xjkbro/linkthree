a2195c2d0adf007f0aec9a928524b4be0d884edd
Jason Kyle De Lara <jason8oh8@gmail.com>
2025-08-04 23:22:33 -0700

    feat: integrate Umami analytics script into RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Background from '@/components/Background'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,13 +9,7 @@ export const metadata = {
   title: 'Jason Kyle De Lara',
   description: 'Linktree clone for Jason Kyle De Lara using ThreeJS and NextJS',
 }
-export function Head() {
-	return (
-		<>
-			<script defer src="https://umami.jkbro.dev/script.js" data-website-id="f953f557-29ee-49d3-800d-5306238b2535"></script>
-		</>
-	)
-}
+
 export default function RootLayout({
   children,
 }: {
@@ -25,6 +20,11 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Background/>
+        <Script 
+          src="https://umami.jkbro.dev/script.js" 
+          data-website-id="f953f557-29ee-49d3-800d-5306238b2535"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   )
